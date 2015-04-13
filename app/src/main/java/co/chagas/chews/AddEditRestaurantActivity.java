f9588615fc5afbd640fe6a4be7750e0051d254f1
Andrea <andrea.lima.chagas@gmail.com>
2015-04-13 22:50:03 +0800

    Latest addition to code.

--- app/src/main/java/co/chagas/chews/AddEditRestaurantActivity.java
+++ app/src/main/java/co/chagas/chews/AddEditRestaurantActivity.java
@@ -1,10 +1,19 @@
 package co.chagas.chews;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RatingBar;
+
+import co.chagas.chews.database.DatabaseConnector;
 
 
 public class AddEditRestaurantActivity extends ActionBarActivity {
@@ -15,8 +24,8 @@ public class AddEditRestaurantActivity extends ActionBarActivity {
     private EditText nameEditText;
     private EditText typeEditText;
     private EditText addressEditText;
-    private EditText drinkEditText;
-    private EditText priceEditText;
+    private CheckBox drinkEditText;
+    private RatingBar priceEditText;
     private EditText commentEditText;
 
     // called when Activity is first started
@@ -26,7 +35,90 @@ public class AddEditRestaurantActivity extends ActionBarActivity {
         setContentView(R.layout.activity_add_edit_restaurant); // inflate the UI
 
         nameEditText = (EditText) findViewById(R.id.nameEditText);
-    }
+        typeEditText = (EditText) findViewById(R.id.typeEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        drinkEditText = (CheckBox) findViewById(R.id.drinkCheckBox);
+        priceEditText = (RatingBar) findViewById(R.id.priceRatingBar);
+        commentEditText = (EditText) findViewById(R.id.commentEditText);
+
+        Bundle extras = getIntent().getExtras(); // get Bundle of extras
+
+        // if there are extras, use them to populate the EditTexts
+        if(extras != null) {
+            rowID = extras.getLong("row_id");
+            nameEditText.setText(extras.getString("name"));
+            typeEditText.setText(extras.getString("type"));
+            addressEditText.setText(extras.getString("address"));
+            drinkEditText.setText(extras.getInt("drink"));
+            priceEditText.setNumStars(extras.getInt("price"));
+            commentEditText.setText(extras.getString("comment"));
+        } // end if
+
+        // set event listener for the Save Restaurant Button
+        Button saveRestaurantButton = (Button) findViewById(R.id.saveRestaurantButton);
+        saveRestaurantButton.setOnClickListener(saveRestaurantButtonClicked);
+    } // end method onCreate
+
+    // respond to event generated when user clicks the Done Button
+    View.OnClickListener saveRestaurantButtonClicked = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(nameEditText.getText().length() != 0) {
+                AsyncTask<Object, Object, Object> saveRestaurantTask =
+                        new AsyncTask<Object, Object, Object>() {
+                            @Override
+                            protected Object doInBackground(Object... params) {
+                                saveRestaurant(); // save restaurant to the database
+                                return null;
+                            } // end method doInBackground
+
+                            @Override
+                        protected void onPostExecute(Object result) {
+                                finish(); // return to the previous Activity
+                            } // end method onPostExecute
+                        }; // end AsyncTask
+
+                // save the restaurant to the database using a separate thread
+                saveRestaurantTask.execute((Object[]) null);
+            } // end if
+            else {
+                // create a new AlertDialog Builder
+                AlertDialog.Builder builder = new AlertDialog.Builder(AddEditRestaurantActivity.this);
+
+                // set dialog title and message, and provide Button to dismiss
+                builder.setTitle(R.string.errorTitle);
+                builder.setMessage(R.string.error_message);
+                builder.setPositiveButton(R.string.errorButton, null);
+                builder.show(); // display the Dialog
+            } // end else
+        } // end method
+    }; // end OnClickListener saveRestaurantButtonClicked
+
+    // saves restaurant information to database
+    private void saveRestaurant() {
+        // get DatabaseConnector to interact with the SQLite database
+        DatabaseConnector databaseConnector = new DatabaseConnector(this);
+
+        if(getIntent().getExtras() == null) {
+            // insert the contact information into the database
+            databaseConnector.insertRestaurant(
+                    nameEditText.getText().toString(),
+                    typeEditText.getText().toString(),
+                    addressEditText.getText().toString(),
+                    drinkEditText.getText().toString(),
+                    priceEditText.getNumStars(),
+                    commentEditText.getText().toString());
+        } // end if
+        else {
+            databaseConnector.updateRestaurant(rowID,
+                    nameEditText.getText().toString(),
+                    typeEditText.getText().toString(),
+                    addressEditText.getText().toString(),
+                    drinkEditText.getText().toString(),
+                    priceEditText.getNumStars(),
+                    commentEditText.getText().toString());
+        } // end else
+    } // end class saveRestaurant
 
 
     @Override
@@ -50,4 +142,4 @@ public class AddEditRestaurantActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+} // end class AddEditRestaurantActivity
--- app/src/main/java/co/chagas/chews/database/DatabaseConnector.java
+++ app/src/main/java/co/chagas/chews/database/DatabaseConnector.java
@@ -1,7 +1,113 @@
 package co.chagas.chews.database;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
+
 /**
  * Created by andreachagas on 12/04/15.
+ *
+ * Provides easy connection and creation of Restaurants database
  */
+
+
 public class DatabaseConnector {
-}
+    // database name
+    private static final String DATABASE_NAME = "Restaurants";
+    private SQLiteDatabase database; // database object
+    private DatabaseOpenHelper databaseOpenHelper; // database helper
+
+    // public constructor for DatabaseConnector
+    public DatabaseConnector(Context context) {
+        // create a new DatabaseOpenHelper
+        databaseOpenHelper = new DatabaseOpenHelper(context, DATABASE_NAME, null, 1);
+    } // end DatabaseConnector constructor
+
+    // open the database connection
+    public void open() throws SQLException {
+        // create or open database for reading/writing
+        database = databaseOpenHelper.getWritableDatabase();
+    } // end method
+
+    // close the database connection
+    public void close() throws SQLException {
+        if(database != null)
+            database.close(); // close the database connection
+    } // end method close
+
+    // insert a new restaurant in the database
+    public void insertRestaurant(String name, String type, String address, String drink, int price,
+                                 String comment) {
+        ContentValues newRestaurant = new ContentValues();
+        newRestaurant.put("name", name);
+        newRestaurant.put("type", type);
+        newRestaurant.put("address", address);
+        newRestaurant.put("drink", drink);
+        newRestaurant.put("price", price);
+        newRestaurant.put("comment", comment);
+        open(); // open the database
+        database.insert("restaurants", null, newRestaurant);
+        close(); // close the database
+    } // end method insertRestaurant
+
+    // insert a new restaurant in the database
+    public void updateRestaurant(long id, String name, String type, String address, String drink, int price,
+                                 String comment){
+        ContentValues newRestaurant = new ContentValues();
+        newRestaurant.put("name", name);
+        newRestaurant.put("type", type);
+        newRestaurant.put("address", address);
+        newRestaurant.put("drink", drink);
+        newRestaurant.put("price", price);
+        newRestaurant.put("comment", comment);
+
+        open();
+        database.update("restaurants", editRestaurant, "_id=" + id, null);
+        close();
+    } // end method updateRestaurant
+
+    // return a Cursor with all restaurant information in the database
+    public Cursor getAllRestaurants() {
+        return database.query("restaurants", new String[] {"_id", "name"}, null, null, null,
+                null, null, "name");
+    } // end method getAllRestaurants
+
+    // get a Cursor containing all information about the contact specified by the given id
+    public Cursor getOneRestaurant(long id) {
+        return database.query("restaurants", null, "_id='" + id, null, null, null, null, null);
+    } // end method getOneRestaurant
+
+    // delete the restaurant specified by the given String name
+    public void deleteRestaurant(long id) {
+        open(); // open database
+        database.delete("restaurants", "_id=" + id, null);
+        close(); // close the database
+    } // end method deleteRestaurant
+
+    private class DatabaseOpenHelper extends SQLiteOpenHelper {
+        // public constructor
+        public DatabaseOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory,
+                                  int version) {
+            super(context, name, factory, version);
+        } // end databaseopenhelper constructor
+
+        // create the restaurants table when the database is created
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            // query to create a new table named restaurants
+            String createQuery = "CREATE TABLE restaurants" +
+                    "(_id integer primary key autoincrement," +
+                    "name TEXT, type TEXT, address TEXT, drink integer, price integer, comment TEXT)";
+            db.execSQL(createQuery); // execute the query
+        } // end method onCreate
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        } // end method onUpgrade
+    } // end class DatabaseOpenHelper
+} // end class DatabaseConnector
